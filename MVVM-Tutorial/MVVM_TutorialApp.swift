45f4b49225bcfe0b210c55dd16a451befc3a08d4
taracell <48975214+taracell@users.noreply.github.com>
2021-02-12 09:03:38 -0800

    Initial Commit

---
+++ MVVM-Tutorial/MVVM_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_TutorialApp.swift
+//  MVVM-Tutorial
+//
+//  Created by Tara on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
